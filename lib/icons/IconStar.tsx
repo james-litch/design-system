a535bd227f79117f25cc80a44e31202e649ad7c5
James Litchfield <james.litchfield99@gmail.com>
2024-07-13 23:46:08 +0100

    Make svgs available when being built

--- lib/icons/IconStar.tsx
+++ lib/icons/IconStar.tsx
@@ -1,6 +1,6 @@
 import type { HTMLAttributes } from "react";
 
 export function IconStar({ ...props }: HTMLAttributes<HTMLImageElement>) {
-  const url = new URL("./star.png", import.meta.url).href;
-  return <img loading="lazy" alt="star" {...props} src={url} />;
+  const url = new URL("../public/icons/star.png", import.meta.url).href;
+  return (<img loading="lazy" alt="star" {...props} src={url} />);
 }
